abb7313647d5d519fb410011ac14e2a8b2b8b60e
Robert Kovacsics <robert.kovacsics@cambridgeconsultants.com>
2024-08-20 23:09:58 +0100

    WIP: nix-ld suggestion

--- nixos/modules/programs/nix-ld.nix
+++ nixos/modules/programs/nix-ld.nix
@@ -1,69 +1,109 @@
 { pkgs, lib, config, ... }:
 let
-  suffixes = [ "nix-ld" "nix-ld-32" ];
-  variant = name: {
-    inherit name;
-    pkgs = { "nix-ld" = pkgs; "nix-ld-32" = pkgs.pkgsi686Linux; }.${name};
-    ldso = { "nix-ld" = "ldso"; "nix-ld-32" = "ldso32"; }.${name};
-    cfg = config.programs."${name}";
-  };
+  inherit (lib)
+    literalExpression
+    mapAttrsToList
+    mergeAttrsList
+    mkEnableOption
+    mkMerge
+    mkOption
+    mkPackageOption
+    types;
 
-  nix-ld-libraries = name: cfg: pkgs: pkgs.buildEnv {
+  cfg = config.programs.nix-ld.systems;
+
+  share-path = system: "share/nix-ld-${system}";
+
+  nix-ld-libraries = system: cfg: cfg.pkgs.buildEnv {
     name = "ld-library-path";
     pathsToLink = [ "/lib" ];
     paths = map lib.getLib cfg.libraries;
     # TODO make glibc here configurable?
     postBuild = ''
-      ln -s ${pkgs.stdenv.cc.bintools.dynamicLinker} $out/share/${name}/lib/ld.so
+      ln -s ${cfg.pkgs.stdenv.cc.bintools.dynamicLinker} $out/${share-path system}/lib/ld.so
     '';
-    extraPrefix = "/share/${name}";
+    extraPrefix = "/${share-path system}";
     ignoreCollisions = true;
   };
 in
 {
   meta.maintainers = [ lib.maintainers.mic92 ];
-  options.programs = lib.genAttrs suffixes (suffix:
-    let inherit (variant suffix) name cfg pkgs; in {
-      enable = lib.mkEnableOption ''nix-ld, Documentation: <https://github.com/Mic92/nix-ld>'';
-      package = lib.mkPackageOption pkgs "nix-ld" { };
-      libraries = lib.mkOption {
-        type = lib.types.listOf lib.types.package;
-        description = "Libraries that automatically become available to all programs. The default set includes common libraries.";
-        default = [ ];
-        defaultText = lib.literalExpression "baseLibraries derived from systemd and nix dependencies.";
+
+  options.programs.nix-ld.systems = mkOption {
+    default = { };
+    description = ''
+      Configure nix-ld for the given system.
+    '';
+    type = types.attrsOf (types.submodule {
+      options = {
+        enable = mkEnableOption "nix-ld for the given system";
+        package = mkPackageOption pkgs "nix-ld" { };
+
+        pkgs = mkOption {
+          type = types.pkgs;
+          default = pkgs;
+          defaultText = "pkgs";
+          description = "Package set to use";
+        };
+
+        ldso = mkOption {
+          type = types.str;
+          description = ''
+            Which runtime loader to override, either `ldso` or `ldso32`, see
+            `environment.ldso` and `environment.ldso32`.
+          '';
+          default = "ldso";
+        };
+
+        libraries = mkOption {
+          type = types.listOf types.package;
+          description = "Libraries that automatically become available to all programs. The default set includes common libraries.";
+          default = [ ];
+          defaultText = literalExpression "baseLibraries derived from systemd and nix dependencies.";
+        };
       };
     });
+  };
 
-  config = lib.mkMerge (lib.forEach suffixes (suffix:
-    let inherit (variant suffix) name cfg pkgs ldso; in lib.mkIf config.programs.${name}.enable {
-      environment.${ldso} = "${cfg.package}/libexec/nix-ld";
+  config =
+    let
+      recursive =
+        # mkMerge
+        mergeAttrsList
+          (mapAttrsToList
+            (system: cfg: {
+              environment.${cfg.ldso} = "${cfg.package}/libexec/nix-ld";
 
-      environment.systemPackages = [ (nix-ld-libraries name cfg pkgs) ];
+              environment.systemPackages = [ (nix-ld-libraries system cfg) ];
 
-      environment.pathsToLink = [ "/share/${name}" ];
+              environment.pathsToLink = [ "/${share-path system}" ];
 
-      environment.variables = {
-        "NIX_LD_${builtins.replaceStrings ["-"] ["_"] pkgs.system}" = "/run/current-system/sw/share/${name}/lib/ld.so";
-        "NIX_LD_LIBRARY_PATH_${builtins.replaceStrings ["-"] ["_"] pkgs.system}" = "/run/current-system/sw/share/${name}/lib";
-      };
+              environment.variables = {
+                "NIX_LD_${builtins.replaceStrings ["-"] ["_"] system}" = "/run/current-system/sw/${share-path system}/lib/ld.so";
+                "NIX_LD_LIBRARY_PATH_${builtins.replaceStrings ["-"] ["_"] system}" = "/run/current-system/sw/${share-path system}/lib";
+              };
 
-      # We currently take all libraries from systemd and nix as the default.
-      # Is there a better list?
-      programs.nix-ld.libraries = with pkgs; [
-        zlib
-        zstd
-        stdenv.cc.cc
-        curl
-        openssl
-        attr
-        libssh
-        bzip2
-        libxml2
-        acl
-        libsodium
-        util-linux
-        xz
-        systemd
-      ];
-    }));
+              # We currently take all libraries from systemd and nix as the default.
+              # Is there a better list?
+              programs.nix-ld.systems.${system}.libraries = with cfg.pkgs; [
+                zlib
+                zstd
+                stdenv.cc.cc
+                curl
+                openssl
+                attr
+                libssh
+                bzip2
+                libxml2
+                acl
+                libsodium
+                util-linux
+                xz
+                systemd
+              ];
+            })
+            cfg);
+    in
+    # recursive;
+    { environment.etc.foo.text = builtins.toJSON recursive; };
 }
